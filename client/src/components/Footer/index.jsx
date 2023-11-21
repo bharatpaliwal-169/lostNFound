317de878de4873d227230779bc466b05fd9b36e7
Bharat Paliwal <18uec169@lnmiit.ac.in>
2023-11-21 21:44:24 +0530

    .

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <>
       <footer className="col-12 text-center text-muted">
-        <p>Created by Team 534</p>
+        <p>Created by Team 535</p>
       </footer>
     </>
   )
